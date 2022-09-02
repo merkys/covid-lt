685a3b7d7192c5dd1b8b700419a7e828896b68dc
Andrius Merkys <andrius.merkys@gmail.com>
2022-09-02 15:14:54 +0300

    No longer sorting chains in the output from pdbio.pdbfile.PDBFile.chains().

--- pdbio/pdbfile.py
+++ pdbio/pdbfile.py
@@ -19,7 +19,7 @@ class PDBFile:
     def __next__(self):
         self.iterchain += 1
         if self.iterchain < len(self.chains()):
-            return self.chain(sorted(self.chains())[self.iterchain])
+            return self.chain(self.chains()[self.iterchain])
         else:
             raise StopIteration()
 
@@ -32,7 +32,7 @@ class PDBFile:
         return Chain(self, chain)
 
     def chains(self):
-        return set([x[21] for x in self.get('ATOM')])
+        return [chain.name for chain in self]
 
     def sequence(self, chain):
         return self.chain(chain).sequence()
